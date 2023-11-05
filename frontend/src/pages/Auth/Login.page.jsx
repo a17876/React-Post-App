be7bd65f9e5079ee082aa633fa5ae3323bdf1000
a17876 <a17876@hanmail.net>
2023-11-04 21:57:20 -0700

    Initial commit

---
+++ frontend/src/pages/Auth/Login.page.jsx
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useBoundStore from "../../store/Store";
+import {
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+} from '@mantine/core';
+import classes from './Login.page.module.css';
+
+const LoginPage = () => {
+  const navigate = useNavigate();
+  const { loginService, authLoading, user } = useBoundStore((state) => state);
+
+  useEffect(() => {
+    if (!!user) {
+      navigate("/posts");
+    }
+  }, [user]);
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    let email = e.target.email?.value;
+    let password = e.target.password?.value;
+    if (!email || !password) return;
+    loginService(email, password);
+  };
+  return (
+    <Container size={420} my={40}>
+      <form onSubmit={onLogin}>
+    <Title ta="center" className={classes.title}>
+      This is the login page
+    </Title>
+    <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+      <TextInput label="Email" placeholder="email" name="email" required />
+      <PasswordInput label="Password" placeholder="password" name="password" required mt="md" />
+      <Group justify="space-between" mt="lg">
+        <Checkbox label="Remember me" />
+      </Group>
+      <Button type="submit" fullWidth mt="xl">
+        Sign in
+      </Button>
+      {authLoading ? <h2>Loading...</h2> : null}
+    </Paper>
+    </form>
+  </Container>
+
+
+  );
+};
+
+export default LoginPage;
