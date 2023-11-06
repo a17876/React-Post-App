7e877151a17f87707256ccbcbec6f512384489a5
a17876 <a17876@hanmail.net>
2023-11-05 19:57:08 -0800

    add url parse to get the index of posts

--- frontend/src/pages/Post/PostDetails.page.jsx
+++ frontend/src/pages/Post/PostDetails.page.jsx
@@ -3,28 +3,33 @@ import DOMAIN from "../../services/endpoint";
 import axios from "axios";
 import { Text, Title, TextInput, Button, Image } from '@mantine/core';
 import classes from './EmailBanner.module.css';
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useLocation  } from "react-router-dom";
+
 
 
 function PostDetailsPage() {
   const posts = useLoaderData();
+  const location = useLocation();
+  const currentURL = location.pathname;
+  const postId = parseInt(currentURL.split("/").pop(), 10) -1 ; 
+  
 
   return (
     <div className={classes.wrapper}>
     <div className={classes.body}>
-      <Title className={classes.title}>{posts[1].title}</Title>
+      <Title className={classes.title}>{posts[postId].title}</Title>
       <Text fw={500} fz="lg" mb={5}>
-      {posts[1].category}
+      {posts[postId].category}
       </Text>
       <Text fz="sm" c="dimmed">
-        {posts[1].content}
+        {posts[postId].content}
       </Text>
 
       <div className={classes.controls}>
         <Button className={classes.control}>Edit</Button>
       </div>
     </div>
-    <Image src={posts[1].image} className={classes.image} />
+    <Image src={posts[postId].image} className={classes.image} />
   </div>
   );
 }
